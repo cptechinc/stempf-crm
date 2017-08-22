728d39a49b81c9eea88fb0412783d00daae461dc
Paul Gomez <pauldro952@outlook.com>
2017-08-22 15:26:21 -0500

    Changed the orderby links
    Have to set query values, then echo the url can't chain it all.
    
    https://github.com/cptechinc/soft-crm/commit/a631e020912f5d57567d35b28f89d7279738eb25

--- site/templates/content/salesrep/orders/orders-thead-rows.php
+++ site/templates/content/salesrep/orders/orders-thead-rows.php
@@ -1,41 +1,32 @@
 <tr>
-   <th>Detail</th>
-   <th>
-       <a href="<?= $ajax->url->query->set("orderby", "orderno-".$nextorder); ?>" class="load-link" <?= $ajax->data; ?>>
-           Order #<?= $orderno_sym; ?>
-       </a>
-   </th>
-   <th>
-       Customer
-   </th>
-
-   <th>
-       <a href="<?= $ajax->url->query->set("orderby", "custpo-".$nextorder); ?>" class="load-link" <?= $ajax->data; ?>>
-           Customer PO: <?= $custpo_sym; ?>
-       </a>
-   </th>
-   <th>Ship-To</th>
-
-   <th>
-       <a href="<?= $ajax->url->query->set("orderby", "subtotal-".$nextorder);?>" class="load-link" <?= $ajax->data; ?>>
-           Order Totals <?= $total_sym; ?>
-       </a>
-   </th>
-
-   <th>
-       <a href="<?= $ajax->url->query->set("orderby", "orderdate-".$nextorder); ?>" class="load-link" <?= $ajax->data; ?>>
-           Order Date: <?= $orderdate_sym; ?>
-       </a>
-   </th>
-   <th class="text-center">
-       <a href="<?= $ajax->url->query->set("orderby", "status-".$nextorder); ?>" class="load-link" <?= $ajax->data; ?>>
-           Status:<?= $status_sym; ?>
-       </a>
-   </th>
-   <th colspan="3"> <a tabindex="0" <?= $legendiconcontent; ?> data-content="<?= $legendcontent; ?>">Icon Definitions</a>
-       <?php if (isset($input->get->orderby)) : ?>
-           <a class="btn btn-warning btn-sm load-link" href="<?= $ajax->url->query->remove("orderby"); ?>" <?= $ajax->data; ?>> (Clear Sort) </a>
-       <?php endif; ?>
-   </th>
-
+    <th>Detail</th>
+        <th>
+        <?php $ajax->url->query->set("orderby", "orderno-".$nextorder); ?>
+        <a href="<?= $ajax->url; ?>" class="load-link" <?= $ajax->data; ?>> Order #<?= $orderno_sym; ?> </a>
+    </th>
+    <th> Customer </th>
+    <th>
+        <?php $ajax->url->query->set("orderby", "orderno-".$nextorder); ?>
+        <a href="<?= $ajax->url; ?>" class="load-link" <?= $ajax->data; ?>>Customer PO: <?= $custpo_sym; ?></a>
+    </th>
+    <th>Ship-To</th>
+    <th>
+        <?php $ajax->url->query->set("orderby", "subtotal-".$nextorder); ?>
+        <a href="<?= $ajax->url; ?>" class="load-link" <?= $ajax->data; ?>>Order Totals <?= $total_sym; ?> </a>
+    </th>
+    <th>
+        <?php $ajax->url->query->set("orderby", "orderdate-".$nextorder); ?>
+        <a href="<?= $ajax->url; ?>" class="load-link" <?= $ajax->data; ?>>Order Date: <?= $orderdate_sym; ?></a>
+    </th>
+    <th class="text-center">
+        <?php $ajax->url->query->set("orderby", "status-".$nextorder); ?>
+        <a href="<?= $ajax->url; ?>" class="load-link" <?= $ajax->data; ?>>Status:<?= $status_sym; ?></a>
+    </th>
+    <th colspan="3">
+        <a tabindex="0" <?= $legendiconcontent; ?> data-content="<?= $legendcontent; ?>">Icon Definitions</a>
+        <?php $ajax->url->query->remove("orderby");  ?>
+        <?php if (isset($input->get->orderby)) : ?>
+            <a class="btn btn-warning btn-sm load-link" href="<?= $ajax->url; ?>" <?= $ajax->data; ?>> (Clear Sort) </a>
+        <?php endif; ?>
+    </th>
 </tr>
