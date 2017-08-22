e884fa407020f656697666b853f40316b531f58b
Paul Gomez <pauldro952@outlook.com>
2017-08-22 14:47:12 -0500

    Removed blank lines
    Removed unused includes
    Add include utf8 class
    
    https://github.com/cptechinc/soft-crm/commit/92f9744971042e63cf29af6fa14b4e93d91bebeb
    
    https://github.com/cptechinc/soft-crm/commit/7d223e8b7a5285be888b4264444eba47ed1890e1

--- site/templates/_init.php
+++ site/templates/_init.php
@@ -14,22 +14,23 @@
 	include_once("./_func.php"); // include our shared functions
 	include_once("./_dbfunc.php");
 
-
 	include_once($config->paths->content."common/initialize.php");
-
 
 	include $config->paths->vendor."cptech/src/UserAction.class.php";
 	include $config->paths->vendor."cptech/src/UserActionPanel.class.php";
 	include $config->paths->vendor."cptech/src/Contact.class.php";
 	include $config->paths->vendor."cptech/src/Table.php";
+	include $config->paths->vendor."cptech/src/utfport.php";
 
 	$config->sessionName = session_name();
-
 	$page->fullURL = new \Purl\Url($page->httpUrl);
-	$page->fullURL->join($config->filename);
+
+	if (!empty($config->filename) && $config->filename != '/') {
+		$page->fullURL->join($config->filename);
+	}
+	
 	$page->querystring = $querystring = $page->fullURL->query;
 	$page->PageURL = $page->httpUrl.'?'.$page->querystring;
-
 
 	$config->styles->append($config->urls->templates.'styles/bootstrap.min.css');
 	$config->styles->append('https://fonts.googleapis.com/icon?family=Material+Icons');
@@ -43,16 +44,14 @@
 	$config->scripts->append($config->urls->templates.'scripts/classes.js');
 	$config->scripts->append($config->urls->templates.'scripts/scripts.js');
 
-
-
 	$user->loggedin = is_valid_login(session_id());
+
 	if ($user->loggedin) {
 		setupuser(session_id());
 	} elseif (strtolower($page->title) != 'login' && strtolower($page->title) != 'redir' ) {
 		header('location: ' . $config->pages->login);
 		exit;
 	}
-
 
 	if ($input->get->modal) {
 		$config->modal = true;
