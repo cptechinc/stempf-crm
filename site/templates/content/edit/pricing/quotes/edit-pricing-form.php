ad224edbd0175308a2e8f405dbfd281b062e85ed
Paul Gomez <pauldro952@outlook.com>
2017-08-27 23:59:00 -0500

    update-item form

--- site/templates/content/edit/pricing/quotes/edit-pricing-form.php
+++ site/templates/content/edit/pricing/quotes/edit-pricing-form.php
@@ -4,6 +4,7 @@
 		$linedetail['shipfrom'] = '';
 	}
 ?>
+
 <?php if ($config->ajax) : ?>
 	<p>
 		<a href="<?php echo $config->filename; ?>" target="_blank"><i class="glyphicon glyphicon-print" aria-hidden="true"></i> View Printable Version</a>
@@ -11,22 +12,25 @@
 		<a href="<?= $config->pages->products.'redir/?action=ii-select&itemID='.urlencode($linedetail['itemid']); ?>" target="_blank"><i class="material-icons" aria-hidden="true">&#xE051;</i> View In II</a>
 	</p>
 <?php endif; ?>
+
 <form action="<?php echo $formaction; ?>" method="post" id="<?= $linedetail['itemid'].'-form'; ?>">
-    <input type="hidden" name="action" value="update-line">
-    <input type="hidden" name="qnbr" value="<?= $qnbr; ?>">
+    <input type="hidden" class="action" name="action" value="update-line">
+    <input type="hidden" name="ordn" value="<?= $ordn; ?>">
+    <input type="hidden" class="listprice" value="<?= formatmoney($linedetail['listprice']); ?>">
     <input type="hidden" class="linenumber" name="linenbr" value="<?= $linedetail['linenbr']; ?>">
+    <input type="hidden" class="originalprice" value="<?= formatmoney($linedetail['price']); ?>">
+    <input type="hidden" class="discountprice" value="<?= formatmoney($linedetail['price']); ?>">
+    <input type="hidden" class="cost" value="<?= formatmoney($linedetail['cost']); ?>">
 	<input type="hidden" class="minprice" value="<?= formatmoney($linedetail['minprice']); ?> ">
-	<?php if ($input->get->text('quote-to-order') == 'true') : ?>
-		<input type="hidden" name="page" value="<?= $config->pages->orderquote."?qnbr=".$qnbr; ?>">
-	<?php endif; ?>
-    <?php if ($soconfig['config']['use_discount'] != 'Y'): ?>
+    <input type="hidden" class="calculate-from" value="percent">
+    <?php if (!$soconfig['config']['use_discount']): ?>
         <input type="hidden" class="discpct" name="discount" value="<?= formatmoney($linedetail['discpct']); ?>">
     <?php endif; ?>
-	<?php if (!$soconfig['config']['change_price']) : ?>
+    <?php if (!$soconfig['config']['change_price']) : ?>
         <input type="hidden" name="price" value="<?= formatmoney($linedetail['price']); ?>">
     <?php endif; ?>
     <div class="row">
-    	<div class="col-sm-8">
+    	<div class="col-sm-8 item-information">
     		<div class="jumbotron item-detail-heading"> <div> <h4>Item Info</h4> </div> </div>
             <?php include $config->paths->content."edit/pricing/item-info.php"; ?>
 
@@ -45,43 +49,31 @@
             <div class="jumbotron item-detail-heading"> <div class=""> <h4>Item Availability</h4> </div> </div>
             <?php include $config->paths->content."edit/pricing/item-stock.php"; ?>
     	</div>
-    	<div class="col-sm-4">
-			<?php
+    	<div class="col-sm-4 item-form">
+    		<h4>Current Price</h4>
+            <?php
                 if ($soconfig['config']['change_price']) {
-                    include $config->paths->content."edit/pricing/quotes/simple/edit-pricing-table.php";
+                    include $config->paths->content.'edit/pricing/tables/price-edit-table.php';
                 } else {
-                    include $config->paths->content."edit/pricing/quotes/static-pricing-table.php";
+                    include $config->paths->content.'edit/pricing/tables/price-static-table.php';
                 }
             ?>
 
-    		<table class="table table-bordered table-striped table-condensed">
+   			<table class="table table-bordered table-striped table-condensed">
 				<tr>
 					<td>Requested Ship Date</td>
 					<td>
 						<div class="input-group date" style="width: 180px;">
 							<?php $name = 'rqstdate'; $value = $linedetail['rshipdate'];?>
 							<?php include $config->paths->content."common/date-picker.php"; ?>
-
 						</div>
 					</td>
 				</tr>
 				<tr>
 					<td>Warehouse</td><td><input type="text" class="form-control input-sm qty <?= $linedetail['itemid']."-whse"; ?>" name="whse" value="<?= $linedetail['whse']; ?>"></td>
 				</tr>
-				<?php if (1 == 100) : ?>
-					<tr>
-						<td>
-							On Order
-						</td>
-						<td>
-							<input type="checkbox" name="form1" id="so-form1" class="check-toggle" data-size="small" data-width="73px" value="Y">
-						</td>
-					</tr>
-				<?php endif; ?>
-				<tr>
-					<td>
-						Special Order
-					</td>
+                <tr>
+					<td>Special Order </td>
 					<td>
 						<select name="specialorder" class="form-control input-sm">
 							<?php foreach ($config->specialordertypes as $spectype => $specdesc) : ?>
@@ -123,16 +115,6 @@
 						<td>Vendor ItemID</td>
 						<td><input type="text" name="itemID" class="form-control input-sm" value="<?= $linedetail['vendoritemid']; ?>"></td>
 					</tr>
-<<<<<<< HEAD
-=======
-					<tr>
-						<td>Desc 1</td>
-						<td><input type="text" name="desc1" class="form-control input-sm" value="<?= $linedetail['desc1']; ?>"></td>
-					</tr>
-					<tr>
-						<td>Desc 2</td>
-						<td><input type="text" name="desc2" class="form-control input-sm" value="<?= $linedetail['desc2']; ?>"></td>
-					</tr>
 					<tr>
 						<td>Group</td>
 						<td>
@@ -149,20 +131,24 @@
                             </select>
 						</td>
 					</tr>
->>>>>>> 0c80bf2... Update for nonstock items
+					<tr>
+						<td>PO Nbr</td>
+						<td><input type="text" name="ponbr" class="form-control input-sm" value="<?= $linedetail['ponbr']; ?>" disabled></td>
+					</tr>
+					<tr>
+						<td>Reference</td>
+						<td><input type="text" name="poref" class="form-control input-sm" value="<?= $linedetail['poref']; ?>"></td>
+					</tr>
 				</table>
 			</div>
+
 
 			<?php if ($linedetail['can-edit']) :?>
 		    	<button type="submit" class="btn btn-success btn-block"><i class="glyphicon glyphicon-floppy-disk" aria-hidden="true"></i> Save Changes</button>
 				<br>
 				<button type="button" class="btn btn-danger btn-block remove-item"><i class="fa fa-trash" aria-hidden="true"></i> Delete Line</button>
 		    <?php endif; ?>
+
     	</div>
     </div>
-
-
-
-
-
 </form>
