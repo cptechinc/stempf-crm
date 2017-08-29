81b2784805186e5974a807f5deb18b6f8faaab8e
Paul Gomez <pauldro952@outlook.com>
2017-08-29 10:38:29 -0500

    Add Task links

--- vendor/cptech/src/UserActionPanel.class.php
+++ vendor/cptech/src/UserActionPanel.class.php
@@ -156,6 +156,11 @@
             return str_replace($this->getactiontypepage(), '{replace}', $addlink);
         }
 
+        function getaddtasktypelink() {
+            $addlink = $this->getaddactionlink();
+            return str_replace($this->getactiontypepage(), 'tasks', $addlink);
+        }
+
 		function getpanelrefreshlink() {
 			$link = '';
 			switch ($this->type) {
