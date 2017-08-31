7c46209629c4d98a87751e90c44646569e9dcb3b
Paul Gomez <pauldro952@outlook.com>
2017-08-31 11:12:55 -0500

    Confirmation Template, started on quote confirmation page

---
+++ site/templates/template-confirmation.php
@@ -0,0 +1,22 @@
+<?php
+    switch ($page->parent->name) { //$page->name is what we are editing
+        case 'order':
+            $ordn = $input->get->text('ordn');
+			$page->body = $config->paths->content."edit/orders/outline.php";
+            break;
+        case 'quote':
+            $qnbr = $input->get->text('qnbr');
+            $page->body = $config->paths->content."confirm/quotes/outline.php";
+            break;
+    }
+ ?>
+ <?php include('./_head.php'); // include header markup ?>
+ 	<div class="jumbotron pagetitle">
+ 		<div class="container">
+ 			<h1><?php echo $page->get('pagetitle|headline|title') ; ?></h1>
+ 		</div>
+ 	</div>
+     <div class="container page" id="edit-page">
+        <?php include ($page->body); ?>
+     </div>
+ <?php include('./_foot.php'); // include footer markup ?>
