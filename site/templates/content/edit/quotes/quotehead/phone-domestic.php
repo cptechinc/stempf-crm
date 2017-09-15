ddc5956d29c0083d121ffbadad639eb9e14005e8
Paul Gomez <pauldro952@outlook.com>
2017-09-15 14:24:16 -0500

    Removed onkeyup=addDashes added .phone-input class
    (cherry picked from commit cptech/soft-crm@e99ad1228fb8962ad29f9c05c360b31626575810)

--- site/templates/content/edit/quotes/quotehead/phone-domestic.php
+++ site/templates/content/edit/quotes/quotehead/phone-domestic.php
@@ -1,8 +1,8 @@
 <tr class="domestic <?php echo $hidden_domestic; ?>">
     <td class="control-label">Contact Phone</td>
-    <td> <input type="text" name="contact-phone" class="form-control input-sm required" id="shiptophone" onKeyup='addDashes(this)' value="<?php echo $quote['telenbr']; ?>"> </td>
+    <td> <input type="text" name="contact-phone" class="form-control input-sm phone-input required" id="shiptophone" value="<?php echo $quote['telenbr']; ?>"> </td>
 </tr>
 <tr class="domestic <?php echo $hidden_domestic; ?>">
     <td class="control-label">Contact Fax</td>
-    <td> <input type="text" name="contact-fax" class="form-control input-sm" onKeyup='addDashes(this)' value="<?php echo $quote['faxnbr']; ?>"> </td>
+    <td> <input type="text" name="contact-fax" class="form-control input-sm phone-input" value="<?php echo $quote['faxnbr']; ?>"> </td>
 </tr>
