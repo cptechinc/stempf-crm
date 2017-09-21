b7272321006f9e3bf6a50619bc985a3c058f7edf
Paul Gomez <pauldro952@outlook.com>
2017-09-20 19:15:05 -0500

    Made Composer class
    (cherry picked from commit cptechinc/soft-crm@c3a111860af13b11fc04d60d3476642b772e2d25)

---
+++ vendor/cptechinc/dpluso-processwire-classes/src/Contact.class.php
@@ -0,0 +1,124 @@
+<?php
+
+    class Contact {
+        public $recno;
+		public $date;
+		public $time;
+		public $splogin1;
+		public $splogin2;
+		public $splogin3;
+		public $custid;
+		public $shiptoid;
+		public $name;
+		public $addr1;
+		public $addr2;
+		public $ccity;
+		public $cst;
+		public $czip;
+		public $cphone;
+		public $ccellphone;
+		public $contact;
+		public $source;
+		public $cphext;
+		public $amountsold;
+		public $timesold;
+		public $lastsaledate;
+		public $email;
+
+        public $hasshipto = false;
+		public $hasphoneextension = false;
+
+        public function __construct() {
+            if ($this->shiptoid != '') { $this->hasshipto = true; }
+			if ($this->cphext != '') { $this->hasphoneextension = true; }
+
+        }
+
+        public function generatecustomerurl() {
+            return wire('config')->pages->customer."redir/?action=load-customer&custID=".urlencode($this->custid);
+        }
+
+        public function generateshiptourl() {
+            return $this->generatecustomerurl() . "&shipID=".urlencode($this->shiptoid);
+        }
+
+		public function generatecustloadurl() {
+			if ($this->hasshipto) {
+				return $this->generateshiptourl();
+			} else {
+				return $this->generatecustomerurl();
+			}
+		}
+
+        public function generatecontacturl() {
+            if ($this->hasshipto) {
+                return wire('config')->pages->customer.urlencode($this->custid) . "/shipto-".urlencode($this->shiptoid)."/contacts/?id=".urlencode($this->contact);
+            } else {
+                return wire('config')->pages->customer.urlencode($this->custid)."/contacts/?id=".urlencode($this->contact);
+            }
+        }
+
+		function generateciloadurl() {
+			 if ($this->hasshipto) {
+                return wire('config')->pages->customer."redir/?action=ci-customer&custID=".urlencode($this->custid)."&shipID=".urlencode($this->shiptoid);
+            } else {
+                return wire('config')->pages->customer."redir/?action=ci-customer&custID=".urlencode($this->custid);
+            }
+		}
+
+		function generateciurl() {
+			 if ($this->hasshipto) {
+                return wire('config')->pages->custinfo.urlencode($this->custid) . "/shipto-".urlencode($this->shiptoid)."/";
+            } else {
+                return wire('config')->pages->custinfo.urlencode($this->custid)."/";
+            }
+		}
+
+        function generateiifunction($function) {
+            switch ($function) {
+                case 'ii':
+                    return "ii_customer('".$this->custid."')";
+                    break;
+                case 'ii-pricing':
+                    return "chooseiipricingcust('".$this->custid."', '')";
+                    break;
+                case 'ii-item-hist':
+                    return "chooseiihistorycust('".$this->custid."', '')";
+                    break;
+            }
+        }
+
+		public function generatephonedisplay() {
+			if ($this->hasphoneextension) {
+				return $this->cphone . ' &nbsp; ' . $this->cphext;
+			} else {
+				return $this->cphone;
+			}
+		}
+
+		public function generatecontactmethodurl($method) {
+			switch ($method) {
+				case 'cell':
+					return "tel:".$this->ccellphone;
+					break;
+				case 'phone':
+					return "tel:".$this->cphone;
+					break;
+				case 'email':
+					return "mailto:".$this->email;
+					break;
+				default:
+					return "tel:".$this->cphone;
+					break;
+			}
+
+		}
+
+		function generateaddress() {
+			return $this->addr1 . ' ' . $this->addr2. ' ' . $this->ccity . ', ' . $this->cst . ' ' . $this->czip;
+		}
+
+    }
+
+
+ ?>
---
+++ vendor/cptechinc/dpluso-processwire-classes/src/DplusDateTime.class.php
@@ -0,0 +1,25 @@
+<?php
+    
+    class DplusDateTime {
+        static $defaultdate = 'm/d/Y';
+        static $defaulttime = 'h:i A';
+        static $fulltimestring = 'YmdHisu';
+        static $shorttimestring = 'Hi';
+
+        static function formatdplustime($time, $formatstring = null, $timestring = null) {
+            $formatstring ? $formatstring = $formatstring : $formatstring = self::$defaulttime;
+            $timestring ? $timestring = $timestring : $timestring = self::$fulltimestring;
+            $formatted = DateTime::createFromFormat($timestring, $time);
+            return $formatted->format($formatstring);
+        }
+
+        static function formatdate($date, $formatstring = null) {
+            $formatstring ? $formatstring = $formatstring : $formatstring = self::$defaultdate;
+            if (strtotime($date) == strtotime("12/31/1969")) {
+    			return '';
+    		} else {
+    			return date($formatstring, strtotime($date));
+    		}
+        }
+    }
+ ?>
