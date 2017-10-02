6d0ee6fe63dc1b2d5a996b3b20c3b4362a2f49ff
Paul Gomez <pauldro952@outlook.com>
2017-10-02 13:29:14 -0500

    Create Order Quote and panel class
    Quote order classes
    (cherry picked from commit cptechinc/soft-crm@65e9f582d08bb67a3a0e9a89f487ed608b503233)

---
+++ vendor/cptechinc/dpluso-processwire-classes/src/Quote.class.php
@@ -0,0 +1,197 @@
+<?php 
+
+    class Quote {
+        public $sessionid;
+        public $recno;
+        public $date;
+        public $time;
+        public $type;
+        public $custid;
+        public $shiptoid;
+        public $custname;
+        public $orderno;
+        public $custpo;
+        public $custref;
+        public $status;
+        public $orderdate;
+        public $careof;
+        public $quotdate;
+        public $invdate;
+        public $shipdate;
+        public $revdate;
+        public $expdate;
+        public $havedoc;
+        public $havetrk;
+        public $odrsubtot;
+        public $odrtax;
+        public $odrfrt;
+        public $odrmis;
+        public $odrtotal;
+        public $havenote;
+        public $editord;
+        public $error;
+        public $errormsg;
+        public $sconame;
+        public $sname;
+        public $saddress;
+        public $saddress2;
+        public $scity;
+        public $sst;
+        public $szip;
+        public $scountry;
+        public $contact;
+        public $phintl;
+        public $phone;
+        public $extention;
+        public $faxnumber;
+        public $email;
+        public $releasenbr;
+        public $shipviacd;
+        public $shipviadesc;
+        public $priccode;
+        public $pricdesc;
+        public $pricdisp;
+        public $taxcode;
+        public $taxcodedesc;
+        public $taxcodedisp;
+        public $termcode;
+        public $termtype;
+        public $termdesc;
+        public $rqstdate;
+        public $shipcom;
+        public $sp1;
+        public $sptname;
+        public $sp2;
+        public $sp2name;
+        public $sp2disp;
+        public $sp3;
+        public $sp3name;
+        public $sp3disp;
+        public $fob;
+        public $deliverydesc;
+        public $whse;
+        public $ccno;
+        public $xpdate;
+        public $ccvalidcode;
+        public $ccapproval;
+        public $costtot;
+        public $totdisc;
+        public $paytype;
+        public $srcdatefrom;
+        public $srcdatethru;
+        public $btname;
+        public $btadr1;
+        public $btadr2;
+        public $btadr3;
+        public $btctry;
+        public $btcity;
+        public $btstate;
+        public $btzip;
+        public $prntfmt;
+        public $prntfmtdisp;
+        public $dummy;
+        
+        public $canedit = true;
+        public $hasnotes = false;
+        
+        
+        public function __construct() {
+            if ($this->editord == 'Y') { $this->canedit = false; }
+            if ($this->havenote == 'Y') { $this->hasnotes = true; }
+        }
+        
+        public static function getlinkarray() {
+            return array(
+                'sessionid' => false,
+                'recno' => false,
+                'date' => false,
+                'time' => false,
+                'type' => false,
+                'custid' => false,
+                'shiptoid' => false,
+                'custname' => false,
+                'orderno' => false,
+                'custpo' => false,
+                'custref' => false,
+                'status' => false,
+                'orderdate' => false,
+                'careof' => false,
+                'quotdate' => false,
+                'invdate' => false,
+                'shipdate' => false,
+                'revdate' => false,
+                'expdate' => false,
+                'havedoc' => false,
+                'havetrk' => false,
+                'odrsubtot' => false,
+                'odrtax' => false,
+                'odrfrt' => false,
+                'odrmis' => false,
+                'havenote' => false,
+                'editord' => false,
+                'error' => false,
+                'errormsg' => false,
+                'sconame' => false,
+                'sname' => false,
+                'saddress' => false,
+                'saddress2' => false,
+                'scity' => false,
+                'sst' => false,
+                'szip' => false,
+                'scountry' => false,
+                'contact' => false,
+                'phintl' => false,
+                'phone' => false,
+                'extension' => false,
+                'faxnumber' => false,
+                'email' => false,
+                'releasenbr' => false,
+                'shipviacd' => false,
+                'shipviadesc' => false,
+                'priccode' => false,
+                'pricdesc' => false,
+                'pricdisp' => false,
+                'taxcode' => false,
+                'taxcodedesc' => false,
+                'taxcodedisp' => false,
+                'termcode' => false,
+                'termtype' => false,
+                'termdesc' => false,
+                'rqstdate' => false,
+                'shipcom' => false,
+                'sp1' => false,
+                'sp1name' => false,
+                'sp2' => false,
+                'sp2name' => false,
+                'sp2disp' => false,
+                'sp3' => false,
+                'sp3name' => false,
+                'sp3disp' => false,
+                'fob' => false,
+                'deliverydesc' => false,
+                'whse' => false,
+                'ccno' => false,
+                'xpdate' => false,
+                'ccvalidcode' => false,
+                'ccapproval' => false,
+                'costtot' => false,
+                'totdisc' => false,
+                'paytype' => false,
+                'srcdatefrom' => false,
+                'srcdatethru' => false,
+                'btname' => false,
+                'btadr1' => false,
+                'btadr2' => false,
+                'btadr3' => false,
+                'btctry' => false,
+                'btcity' => false,
+                'btstate' => false,
+                'btzip' => false,
+                'prntfmt' => false,
+                'prntfmtdisp' => false,
+                'dummy' => false,
+            );
+        }
+    }
+    
+    
---
+++ vendor/cptechinc/dpluso-processwire-classes/src/QuotePanel.class.php
@@ -0,0 +1,103 @@
+<?php 
+
+    class QuotePanel {
+        public $type = 'cust';
+        public $modal,
+        public $focus,
+        public $throughajax;
+        public $collapse = 'collapse'
+        public $orderby,
+        public $orderbystring,
+        public $sortrule,
+        public $nextorder,
+        public $orderby,
+        public $custID,
+        public $shipID,
+        public $count;
+        public $quotes = array();
+        
+        public function __construct($type, $orderbystring, $modal, $loadinto, $throughajax) {
+        	$this->type = $type;
+            $this->orderbystring = $orderbystring;
+        	
+        	if ($throughajax) {
+        		$this->collapse = '';
+        	} else {
+        		$this->collapse = 'collapse';
+        	}
+        	
+        	$this->loadinto = $this->focus = $loadinto;
+        	
+        	$this->data = 'data-loadinto="'.$this->loadinto.'" data-focus="'.$this->focus.'"';
+        }
+
+        public function setupcustomerpanel($custID, $shipID) {
+        	$this->custID = $custID;
+        	$this->shipID = $shipID;
+        }
+        
+        public function setuporderby() {
+            if (!empty($this->orderbystring)) {
+        		$orderby_array = explode('-', $this->orderbystring);
+        		$this->orderby = $orderby_array[0];
+        		if ($orderby_array[0] != '') {
+        			$this->sortrule = $orderby_array[1];
+        			$this->nextorder = get_sorting_rule($orderby, $this->sortrule, $this->orderby);
+        		} else {
+        			$this->sortrule = "ASC";
+        			$this->nextorder = "DESC";
+        		}
+                // TODO: add logic for adding orderby to url
+        		// $sortlinkaddon = "&orderby=".$this->orderbystring;
+        	} else {
+        		$this->orderby = false;
+        		$this->sortrule = false;
+        		$this->nextorder = "ASC";
+                // TODO: add logic for adding orderby to url
+        		// $sortlinkaddon = '';
+        	}
+        }
+        
+        public function createsalesordericonlegend() {
+            $legendcontent = "<span class='glyphicon glyphicon-shopping-cart' title='re-order'></span> = Re-order whole order <br>";
+        	$legendcontent .= "<span class='glyphicon glyphicon-folder-open' title='Click to view Documents'></span> &nbsp; = Documents <br>";
+        	$legendcontent .= "<span class='glyphicon glyphicon-plane hover' title='Click to view Tracking'></span> = Tracking <br>";
+        	$legendcontent .= "<span class='glyphicon glyphicon-list-alt' title='View notes'></span> = Notes <br>";
+        	$legendcontent .= "<span class='glyphicon glyphicon-pencil' title='Edit this Order'></span> = Edit Order<br>";
+        	$legendiconcontent = 'class="btn btn-sm btn-info" role="button" data-toggle="popover" data-placement="bottom" data-trigger="focus" data-html="true" title="Icon Definition"';
+            
+            // TODO: look at legend content
+            return $legendcontent;
+        }
+        
+        public function get_symbols($orderby, $match, $page_orderby) {
+    		$symbol = "";
+    		if ($orderby == $match) {
+    			if ($page_orderby == "ASC") {
+    				$symbol = "&#x25B2;";
+    				$symbol = "<span class='glyphicon glyphicon-arrow-up'></span>";
+    			} else {
+    				$symbol = "&#x25BC;";
+    				$symbol = "<span class='glyphicon glyphicon-arrow-down'></span>";
+    			}
+    		}
+    		return $symbol;
+    	}
+        
+        // TODO: look at function
+        public function get_sorting_rule($orderingby, $sort, $orderby) {
+    		if ($orderingby != $orderby || $sort == false) {
+    			$sortrule = "ASC";
+    		} else {
+    			switch ($sort) {
+    				case 'ASC':
+    					$sortrule = 'DESC';
+    					break;
+    				case 'DESC':
+    					$sortrule = 'ASC';
+    					break;
+    			}
+    		}
+    		return $sortrule;
+    	}
+    }
