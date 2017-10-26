8493a13a435437cfcf7acc3ce96b339ff4f6d3ce
Paul Gomez <pauldro952@outlook.com>
2017-10-26 09:21:45 -0500

    Emptied Archive directory
    (cherry picked from commit cptechinc/soft-crm@ec3f724acc6321d215b66701aab6a6bffe3b58e3)

--- site/templates/content/archive/crmnotes/crm/_pages/cust-page/notes-panel.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$notepanel = new NotePanel('cust', '#notes-panel', '#notes-panel', '#ajax-modal', $config->ajax);
-	$notepanel->setupcustomerpanel($custID, $shipID);
-	$notepanel->count = getlinkednotescount($notepanel->getarraylinks(), false);
-	include $config->paths->content."notes/crm/notes-panel.php";
-?>
--- site/templates/content/archive/crmnotes/crm/_pages/dashboard-page/notes/notes-panel.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$notepanel = new NotePanel('user', '#notes-panel', '#notes-panel', '#ajax-modal', $config->ajax);
-	$notepanel->count = getlinkednotescount($notepanel->getarraylinks(), false);
-	include $config->paths->content."notes/crm/notes-panel.php";
-?>
