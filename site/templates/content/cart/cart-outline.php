b57a129d483e0e3ed72da8bfd55495b1ee233895
Paul Gomez <pauldro952@outlook.com>
2017-11-20 12:13:03 -0600

    Cart ouline change
    (cherry picked from commit cptechinc/soft-crm@4598ca75663606dbc379185c3e09208a0fa0f87a)

--- site/templates/content/cart/cart-outline.php
+++ site/templates/content/cart/cart-outline.php
@@ -1,6 +1,18 @@
 <?php $carthead = getcarthead(session_id(), false); ?>
 <?php include $config->paths->content."/cart/cart-details.php"; ?>
 <br>
-<a href="<?php echo $config->pages->cart."redir/?action=create-sales-order"; ?>" class="btn btn-success">Create Sales Order</a>
+<a href="<?php echo $config->pages->cart."redir/?action=create-sales-order"; ?>" class="btn btn-success">
+    <span class="fa-stack fa-md">
+      <i class="fa fa-usd fa-stack-1x"></i>
+      <i class="fa fa-file-o fa-stack-2x"></i>
+    </span>
+    Create Sales Order
+</a>
 
-<a href="<?php echo $config->pages->cart."redir/?action=create-quote"; ?>" class="btn btn-success">Create Quote</a>
+<a href="<?php echo $config->pages->cart."redir/?action=create-quote"; ?>" class="btn btn- btn-success">
+    <span class="fa-stack fa-md" aria-hidden="true">
+      <i class="fa fa-quote-left fa-stack-1x"></i>
+      <i class="fa fa-file-o fa-stack-2x"></i>
+    </span>
+    Create Quote
+</a>
